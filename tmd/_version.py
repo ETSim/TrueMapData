d9364feb57dcfa6807a10f164095462063ca88bf
Antoine Boucher <antoine.bou13@gmail.com>
2025-03-10 22:16:20 -0400

    Update _version.py

--- tmd/_version.py
+++ tmd/_version.py
@@ -1,4 +1,4 @@
-"""Version information."""ools-scm
+"""Version information."""
 # don't change, don't track in version control
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __all__ = ["__version__"] "version", "version_tuple"]
